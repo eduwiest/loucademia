83bb107ef681f39d22482504b59b362f69ea5d89
edu.wiest <edu.wiest@gmail.com>
2022-06-21 22:59:56 -0300

    Envio do Projeto

---
+++ src/main/java/br/com/softblue/loucademia/application/service/AlunoService.java
@@ -0,0 +1,60 @@
+package br.com.softblue.loucademia.application.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
+import org.picketbox.util.StringUtil;
+
+import br.com.softblue.loucademia.application.uitl.StringUtils;
+import br.com.softblue.loucademia.application.uitl.Validation;
+import br.com.softblue.loucademia.application.uitl.ValidationException;
+import br.com.softblue.loucademia.domain.aluno.Aluno;
+import br.com.softblue.loucademia.domain.aluno.AlunoRepository;
+
+@Stateless
+public class AlunoService {
+
+	@EJB
+	private AlunoRepository alunoRepository;
+	
+	public void createOrUpdate(Aluno aluno) {
+		if(StringUtils.isEmpty(aluno.getMatricula())) {
+			create(aluno);
+		}else {
+			update(aluno);
+		}
+	}
+	
+	private void create(Aluno aluno) {
+		Validation.assertNotEmpty(aluno);
+		
+		String maxMatricula = alunoRepository.getMaxMatriculaAno();
+		aluno.gerarMatricula(maxMatricula);
+		alunoRepository.store(aluno);
+	}
+	
+	public void delete(String matricula) {
+		alunoRepository.delete(matricula);
+	}
+	
+	private void update(Aluno aluno) {
+		Validation.assertNotEmpty(aluno);
+		Validation.assertNotEmpty(aluno.getMatricula());		
+		alunoRepository.update(aluno);		
+	}
+	
+	public Aluno findByMatricula(String matricula) {
+		return alunoRepository.findByMatricula(matricula);
+	}
+	
+	public List<Aluno> listAlunos(String matricula, String nome, Integer rg, Integer telefone){
+		if(StringUtils.isEmpty(matricula) && StringUtils.isEmpty(nome) && rg == null & telefone == null) {
+			throw new ValidationException("Pelo menos um critério de pesquisa deve ser preenchido!");
+		}
+		
+		return alunoRepository.listAlunos(matricula, nome, rg, telefone);
+	}
+}
---
+++ src/main/java/br/com/softblue/loucademia/interfaces/aluno/web/AlunoBean.java
@@ -0,0 +1,65 @@
+package br.com.softblue.loucademia.interfaces.aluno.web;
+
+import java.io.Serializable;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.softblue.loucademia.application.service.AlunoService;
+import br.com.softblue.loucademia.application.uitl.StringUtils;
+import br.com.softblue.loucademia.domain.aluno.Aluno;
+
+@Named
+@RequestScoped
+public class AlunoBean implements Serializable {
+	
+	@EJB
+	private AlunoService alunoService;
+	
+	@Inject
+	private FacesContext facesContext;
+
+	private Aluno aluno = new Aluno();
+	
+	private String matricula;
+	
+	private String titulo = "Novo Aluno";
+	
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void carregar() {
+		if(!StringUtils.isEmpty(matricula)) {
+			aluno = alunoService.findByMatricula(matricula);
+			titulo = "Alterar Aluno";
+		}
+	}
+	
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String gravar() {
+		alunoService.createOrUpdate(aluno);
+		facesContext.addMessage(null, new FacesMessage("Dados gravados com sucesso!"));
+		return null;
+	}
+	
+	public Aluno getAluno() {
+		return aluno;
+	}
+	
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+
+}
