556f8fd63850128aff0e9d62e66cd69734374124
edu.wiest <edu.wiest@gmail.com>
2022-06-22 00:44:48 -0300

    implementando registro de aceso parte 1

--- src/main/java/br/com/softblue/loucademia/interfaces/acesso/web/ControleAcessoBean.java
+++ src/main/java/br/com/softblue/loucademia/interfaces/acesso/web/ControleAcessoBean.java
@@ -2,17 +2,49 @@ package br.com.softblue.loucademia.interfaces.acesso.web;
 
 import java.io.Serializable;
 
+import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import br.com.softblue.loucademia.application.service.AcessoService;
+import br.com.softblue.loucademia.application.uitl.ValidationException;
+import br.com.softblue.loucademia.domain.acesso.TipoAcesso;
 
 @Named
 @RequestScoped
 public class ControleAcessoBean implements Serializable{
 
+	@EJB
+	private AcessoService acessoService;
+	
+	@Inject
+	private FacesContext facesContext;
+	
 	private String matricula;
 	private Integer rg;
 	
 	public String registrarAcesso() {
+		TipoAcesso tipoAcesso;
+		try {
+			tipoAcesso = acessoService.registrarAcesso(matricula, rg);
+		}catch (ValidationException e){
+			facesContext.addMessage(null, new FacesMessage(e.getMessage()));
+			return null;
+		}
+		
+		String msg;
+		if(tipoAcesso == TipoAcesso.Entrada) {
+			msg = "Entrada registrada!";
+		}else if (tipoAcesso == TipoAcesso.Saida) {
+			msg = "Saida registrada!";
+		}else {
+			msg = "Dados de registro de acesso inconsistentes";
+		}
+		
+		facesContext.addMessage(null, new FacesMessage(msg));		
 		return null;
 	}
 	
